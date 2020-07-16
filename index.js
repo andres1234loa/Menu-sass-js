6bd0a4b965dfb7fa844acc026c78f8d86b1ece67
Andres Loaiza <chelinandres@outlook.com>
2020-07-16 09:26:44 -0500

    Add-firtsCommit

---
+++ index.js
@@ -0,0 +1,33 @@
+window.addEventListener("resize",function() 
+         {
+            llamar()
+            
+         });
+          llamar()
+         function llamar()
+         {
+           if(outerWidth <= 816)
+           {
+              
+              
+              
+//                 const elemento1=document.querySelectorAll(".footer .redes img");
+//                 for(var z=0;z<=elemento1.length;z++)
+//                 {
+//                   //el if hace que no se produzca el error 
+// // Cannot read property 'addEventListener' of undefined  at llamar (index.js:19)
+//                   if(elemento1[z])
+//                   {
+//                     elemento1[z].addEventListener("click",escucha);
+//                   }
+          }
+              
+                
+                // elemento2.addEventListener("click",escucha())
+              
+              
+           }
+          
+         
+
+         
